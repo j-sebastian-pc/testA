186892db3b5ae69599508fbee9ddf2edc982ee32
Sebastian Parada C <111198689+j-sebastian-pc@users.noreply.github.com>
2023-11-16 10:32:41 -0500

    Update index.spec.js
    get

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -2,7 +2,30 @@ import app from "../src/app.js";
 import request from 'supertest';
 
 describe("GET /tasks", () => {
-    // Your existing GET tests go here
+    const newTask = {
+        title: "title task",
+        description: "test Description", 
+    };
+
+    // debería responder con un 200 de estado
+    test('should respond with a 200 status code', async () => {
+        const response = await request(app).post("/tasks").send(newTask);
+        expect(response.statusCode).toBe(200);
+    });
+
+    // esto debería responder con un json object
+    test('should have a content-type: application/json in header', async () => {
+        const response = await request(app).post('/tasks').send(newTask);
+
+        expect(response.headers["content-type"]).toEqual(expect.stringContaining("json"));
+    });
+
+    // comprobar si responde con un objeto json conteniendo una nueva tarea con id
+    test("should respond with a task ID", async () => {
+        const response = await request(app).post("/tasks").send(newTask);
+
+        expect(response.body.id).toBeDefined();
+    });
 });
 
 describe('POST /tasks', () => {
@@ -10,7 +33,7 @@ describe('POST /tasks', () => {
         describe('when and description is missing', () => {
             test('should respond with a 400 status code', async () => {
                 const response = await request(app).post('/tasks').send({});
-                expect(response.statusCode).toBe(400); // Fix the typo here
+                expect(response.statusCode).toBe(400); 
             });
         });
     });
